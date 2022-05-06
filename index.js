e74eb319e7638125fb57cd6f4330a036b331389d
Eliran Natan <eliran.natan.87@gmail.com>
2022-05-07 01:56:27 +0300

    sds

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const subscribe = require("./api/subscribe");
+const product = require("./api/product");
 
 app.use(express.json({ extended: false }));
 
-//app.use("/api/product", product);
-app.use("/api/subscribe", subscribe);
+app.use("/api/product", product);
+//app.use("/api/subscribe", subscribe);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
